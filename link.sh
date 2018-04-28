e5a82a05a334803b1e2fd543212bdd6720e9f054
nomurakkooo <b1519008@planet.kanazawa-it.ac.jp>
2018-04-29 02:10:20 +0900

    add link.sh

---
+++ link.sh
@@ -0,0 +1,23 @@
+#!/bin/fish
+
+set -u 
+DOT_DIRECTORY="${HOME}/dotfiles"
+DOT_CONFIG_DIRECTORY=".config"
+
+echo "link home directory dotfiles"
+cd ${DOT_DIRECTORY}
+
+for f in _??*; do
+    # [ "$f" = ".git" ] && continue
+    # [ "$f" = ".congfig" ] && continue
+    ln -snfv ${DOT_DIRECTORY}/${f} ${HOME}/.${f:1}
+done
+
+echo "link .config directory dotfiles"
+cd ${DOT_DIRECTORY}/${DOT_CONFIG_DIRECTORY}
+
+for file in `\find . -maxdepth 8 -type f`; do
+    ln -snfv ${DOT_DIRECTORY}/${DOT_CONFIG_DIRECTORY}${file:2} ${HOME}/S{DOT_CONFIG_DIRECTORY}/${file:2}
+done
+
+echo "linked dotfiles complete!"
