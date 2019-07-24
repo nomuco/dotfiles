47fbc82e8b0ca200b509c0811bfb2fc5a3d02d4f
nomuco <b1519008@planet.kanazawa-it.ac.jp>
2019-07-24 22:34:24 +0900

    add/install.sh

---
+++ shfiles/install.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+
+cat << END
+
+_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/
+
+START SET UP!
+
+_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/
+
+END
+
+echo "homebrew install start"
+
+sh ./homebrew_install.sh
+
+echo "set up jenv for java varsions"
+
+sh ./jsetup.sh
+
+echo "set up zsh"
+
+sh ./default_to_zsh.sh
+
+echo "start link to dotfiles"
+
+sh ./link.sh
+
+cat << END
+
+_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/
+
+SET UP COMPLETE! bye.
+
+_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/
+
+END
